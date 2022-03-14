41a05a54d9f01f7fd9d3ee23de6a7d3ae6bad6ed
pmadinga <phiwen2@gmail.com>
2022-03-14 22:50:25 +0200

    Renamed url

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,21 +1,25 @@
 import Link from 'next/link';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import * as styles from '../styles/Navbar.module.css';
+
 const SiteHeader = () => {
     return (
-        <Navbar expand="md">
+        <Navbar expand="md" sticky='top' className={styles.navbar}>
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="/">Phiwa</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                    <Link href={'/'} passHref>
-                        <Nav.Link>Home</Nav.Link>
-                    </Link>
-                    <Link href={'/about'} passHref>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                    </Link>
-                    
-                </Nav>
+                    <Nav className="ms-auto">
+                        <Link href={'/'} passHref>
+                            <Nav.Link>Home</Nav.Link>
+                        </Link>
+                        <Link href={'/about'} passHref>
+                            <Nav.Link>About</Nav.Link>
+                        </Link>
+                        <Link href={'/projects'} passHref>
+                            <Nav.Link>Projects</Nav.Link>
+                        </Link>
+                    </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head  from 'next/head';
-import  *as styles from '../styles/Home.module.css';
+import { Container } from 'react-bootstrap';
+import  * as styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -8,7 +9,11 @@ export default function Home() {
         <title>Phiwa | Home</title>
       </Head>
       <header className={styles.header}>
-        
+        <Container>
+          <section className={styles.headline}>
+            <h1>Developer</h1>
+          </section>
+        </Container>
       </header>
     </>
   );
